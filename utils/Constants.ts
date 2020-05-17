c6a0fedc297c23cfd1860c41156202b2198706a4
GokulGangadharan123 <gokul.gangadharan@cognitiveclouds.com>
2020-05-17 14:22:36 +0530

    first commit

---
+++ utils/Constants.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: '#02bcd1',
+  secondary: '#ecf0f1',
+  statusBar: '#017a92',
+  bgColor: '#f5f5f5',
+  white: '#ffffff',
+  black: '#37474f',
+  disabled: '#cfd8dc',
+  hint: '#78909c',
+  green: '#2ecc71',
+  red: '#c0392b'
+};
